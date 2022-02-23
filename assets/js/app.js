9144a771a6ad25421129de4cc18aa85a9f65c737
tinachats <marshallchaterera@gmail.com>
2022-02-24 01:25:39 +0200

    Payment methods UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,153 @@
         });
 })()
 
+// Current tab is set to be the first tab (0)
+let currentTab = 0;
+
+// Display the current tab
+showTab(currentTab);
+
+function showTab(n) {
+    // This function will display the specified tab of the form ...
+    var x = document.getElementsByClassName('tab');
+
+    x[n].style.display = 'block';
+
+    // ... and fix the Previous/Next buttons:
+    if (n == 0) {
+        document.getElementById('continue-shopping-btn').style.display = 'inline';
+        document.getElementById('back-btn').style.display = 'none';
+    } else if (n == 1) {
+        document.getElementById('continue-shopping-btn').style.display = 'none';
+        document.getElementById('back-btn').style.display = 'inline';
+        document.getElementById('prev-btn').innerHTML = 'Return to Delivery';
+    } else {
+        document.getElementById('continue-shopping-btn').style.display = 'none';
+        document.getElementById('back-btn').style.display = 'inline';
+        document.getElementById('prev-btn').innerHTML = 'Return to Payment';
+    }
+
+    if (n == (x.length - 1)) {
+        document.getElementById('next-btn').innerHTML = 'Place Order';
+    } else {
+        document.getElementById('next-btn').innerHTML = 'Continue';
+    }
+
+
+    // ... and run a function that displays the correct step indicator:
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName('tab');
+
+    // Exit the function if any field in the current tab is invalid:
+    if (n == 1 && !validateForm()) return false;
+
+    // Hide the current tab:
+    x[currentTab].style.display = 'none';
+
+    // Increase or decrease the current tab by 1:
+    currentTab += n;
+
+    // if you have reached the end of the form... :
+
+    if (currentTab >= x.length) {
+        // Make the last tab show
+        showTab(2);
+
+        //...the form gets submitted:
+        $('#checkout-form').on('submit', function(e) {
+            e.preventDefault();
+            var form_data = $(this).serialize();
+            processingAlert('Please wait while we process your order.');
+
+            // Create an instance of the Stripe object with your publishable API key
+            var stripe = Stripe('pk_test_51IpowVFPOzPM0nADVjHcbqLOBjqfRcuoxfHAzj7eeVSVMgQKexQu9mOegcfE1qNINf2J1jzxpHqC6AzbfBbFEtcX00dwY3ixUR');
+
+            // Set up Stripe.js and Elements to use in checkout form
+            var elements = stripe.elements();
+            var card = elements.create('card', {
+                iconStyle: 'solid',
+                locale: 'auto',
+                style: {
+                    base: {
+                        iconColor: '#c4f0ff',
+                        color: '#fff',
+                        fontWeight: 500,
+                        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+                        fontSize: '16px',
+                        fontSmoothing: 'antialiased',
+
+                        ':-webkit-autofill': {
+                            color: '#fce883',
+                        },
+                        '::placeholder': {
+                            color: '#87BBFD',
+                        },
+                    },
+                    invalid: {
+                        iconColor: '#FFC7EE',
+                        color: '#FFC7EE',
+                    },
+                },
+            });
+            card.mount("#payment-card");
+        });
+        return false;
+    }
+
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+
+    // Preview entered details
+    checkoutPreview();
+}
+
+function validateForm() {
+    // This function deals with validation of the form fields
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName('tab');
+    y = x[currentTab].getElementsByClassName('control-input');
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == '') {
+            // add an "invalid" class to the field:
+            y[i].className += ' invalid';
+            // and set the current valid status to false:
+            valid = false;
+        }
+    }
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid == true) {
+        document.getElementsByClassName('step-indicator')[currentTab].className += ' finish';
+    }
+    return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i, x = document.getElementsByClassName('step-indicator');
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(' active', '');
+        x[i].className = x[i].className.replace(' completed', '');
+    }
+    //... and adds the "active" class to the current step:
+    x[n].className += ' active';
+
+    //... and adds the "completed" class to the previous step:
+    for (i = 1; i < x.length; i++) {
+        if (n - i >= 0) {
+            x[n - i].className += ' completed';
+        }
+
+        // Preview entered details
+        checkoutPreview();
+    }
+}
+
 // RateYo Star Rating
 function starRating(el, rating, size) {
     $(el).rateYo({
